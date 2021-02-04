3d0ce5e2727ab9e67dd83c42e77314d00eb341ff
fig77 <fig.igle@gmail.com>
2021-02-04 17:39:37 -0300

    Solved false bug, added printc func, changed alg to basic no vector version

---
+++ C/cplus/sorting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+void printArray(int arg[], unsigned int l) {
+  for(unsigned int i = 0; i < l - 1; i++){
+    cout << arg[i] << " ";
+    cout << endl;
+  }
+}
+
+void bubbleSort(int arg[], unsigned int l) {
+  bool sorted=false;
+  while(sorted == false) {
+    sorted = true;
+    for(unsigned int i = 0; i < l - 1; i++){
+      if(arg[i] > arg[i+1]){
+        int aux = arg[i+1];
+        arg[i+1] = arg[i];
+        arg[i] = aux;
+        sorted = false;
+      }
+    }
+  }
+  printArray(arg, l);
+}
