c09caefad763c81d6edac975747450739990ea8c
fig77 <fig.igle@gmail.com>
2021-02-06 09:46:36 -0300

    avg_of_3_flwchrt

--- C/cplus/bplus_jigsaw.cpp
+++ C/cplus/bplus_jigsaw.cpp
@@ -1,3 +1,10 @@
 /*This set of basic problems were featured on jigsaw webconference, is a good set of problem for anywone introducing to programming. Yes, you don't need a dynamic typed
 coding language to learn, and then wonder what tf is an unsigned integer or why some problems have data constraints that you never care.*/
 
+
+/* avg of three */
+
+int avrg_of_thr(int x, int y, int z) { return ((x+y+z)/3); } /* flow chart */
+
+
+
