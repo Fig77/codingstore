50fbeab4850842b8ac503e97557720753b38d5b9
fig77 <fig.igle@gmail.com>
2021-02-06 09:43:27 -0300

    jigsawcpp

---
+++ C/cplus/bplus_jigsaw.cpp
@@ -0,0 +1,3 @@
+/*This set of basic problems were featured on jigsaw webconference, is a good set of problem for anywone introducing to programming. Yes, you don't need a dynamic typed
+coding language to learn, and then wonder what tf is an unsigned integer or why some problems have data constraints that you never care.*/
+
