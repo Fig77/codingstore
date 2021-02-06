07922e0b9c4a57f9e3cf91382c04b52c0ea399bb
fig77 <fig.igle@gmail.com>
2021-02-06 11:06:09 -0300

    Added qqk sort with partition ar, ruby

--- Ruby/sortingalg.rb
+++ Ruby/sortingalg.rb
@@ -19,11 +19,31 @@ end
 # def counting_sort(array: []): Nil -> 
 
 def counting_sort(array = [])
-  counter_array = Array.new(array.length, 0)
+  counter_array = Array.new(0, array.length)
   array.each { |x| counter_array[x] += 1 }
   counter_array.each { |x,i| array[i] = x }
 end
 
+def qck_sort(array = [], min = 0, max = nil)
+  return [] if array.empty?
+  max = array.length - 1 if max === nil
+
+  if min < max
+    pivot = partition(array, min, max)
+    quicksort(array, min, pivot - 1)
+    quicksort(array, pivot + 1, max)
+  end
+
+  return array
+end
 
 
-
+def partition(array, min, max)
+  pivot, i = min, min
+  while i < max do
+    i, pivot = pivot, i if array[i] <= array[max]   
+    i += 1
+  end
+  max, pivot = pivot, max
+  return pivot
+end
